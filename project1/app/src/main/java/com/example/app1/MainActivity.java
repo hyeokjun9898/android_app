1ea31d59479b10908f131bcde88235858fbe290f
hyeokjun <gurwns876@naver.com>
2022-03-24 13:56:35 +0900

    레이아웃 설명

--- project1/app/src/main/java/com/example/app1/MainActivity.java
+++ project1/app/src/main/java/com/example/app1/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.app1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_relation);
+
+
+    Button b1 = (Button) findViewById(R.id.button1);
+    b1.setText("코드에서도 변경 가능");
+
+    Button b2 = (Button) findViewById(R.id.button2);
+    b2.setEnabled(false);
+
+
     }
 }
